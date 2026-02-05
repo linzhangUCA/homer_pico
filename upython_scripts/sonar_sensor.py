c253c5ac4e62eecdba88c838e071aa9c3c8260be
linzhanguca <lzhang12@uca.edu>
2026-02-05 09:52:12 -0600

    rename ultrasonic_sensor -> sonar_sensor with HCSR04 as class name

--- upython_scripts/sonar_sensor.py
+++ upython_scripts/sonar_sensor.py
@@ -1,13 +1,16 @@
 from machine import Pin, PWM, reset
 from time import ticks_us
 
-class UltrasonicSensor:
+
+class HCSR04:
     def __init__(self, echo_id, trig_id):
         self.echo_pin = Pin(echo_id, Pin.IN, Pin.PULL_DOWN)
-        self.echo_pin.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self._echo_handler)
+        self.echo_pin.irq(
+            trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self._echo_handler
+        )
         self.trig_pin = PWM(Pin(trig_id), freq=15, duty_ns=10_000)  # 10ms pulse @ 15Hz
         # Variables
-        self.distance = 0.
+        self.distance = 0.0
         self.start_time = None
 
     def _echo_handler(self, pin):
@@ -20,10 +23,11 @@ class UltrasonicSensor:
             else:  # dt < 0.1 ms or dt > 38 ms
                 self.distance = 0.0  # no detection
 
+
 if __name__ == "__main__":
     from time import sleep_ms
 
-    sensor = UltrasonicSensor(echo_id=21, trig_id=22)
+    sensor = HCSR04(echo_id=21, trig_id=22)
 
     try:
         while True:
