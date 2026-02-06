118269a030761e75a0cbb1437f5e91c9ce683a9e
linzhanguca <lzhang12@uca.edu>
2026-02-05 21:10:32 -0600

    add all sensors to pico messenger

--- upython_scripts/pico_messenger.py
+++ upython_scripts/pico_messenger.py
@@ -1,24 +1,26 @@
 import sys
-from utime import time, ticks_us, ticks_diff
+from utime import ticks_us, ticks_diff
 import select
 from machine import freq
 from diff_drive_controller import DiffDriveController
-from ultrasonic_sensor import UltrasonicSensor
+from sonar_sensor import HCSR04
+from imu import MPU6050
 
 # SETUP
 # Overclock
 freq(240_000_000)  # Pico2 original: 150_000_000
 # Instantiate robot
-dist_sensor = UltrasonicSensor(echo_id=21, trig_id=22)
+distance_sensor = HCSR04(echo_id=21, trig_id=22)
+motion_sensor = MPU6050(scl_id=9, sda_id=8, i2c_addr=0x68)
 mobile_base = DiffDriveController(
     left_wheel_ids=((16, 17, 18), (19, 20)),
     right_wheel_ids=((15, 14, 13), (12, 11)),
 )
 mobile_base.awaken()
-pico_messenger = select.poll()  # create a poll object 
-pico_messenger.register(sys.stdin, select.POLLIN) # peek at serial port input
+pico_messenger = select.poll()  # create a poll object
+pico_messenger.register(sys.stdin, select.POLLIN)  # peek at serial port input
 # Constants
-tx_period_us = 16_667  # 60Hz
+tx_period_us = 16_667  # ~60Hz
 # Variables
 targ_lin_vel, targ_ang_vel = 0.0, 0.0
 last_us = ticks_us()
@@ -29,10 +31,15 @@ while True:
     # Transmit data (TX)
     now_us = ticks_us()
     if ticks_diff(now_us, last_us) >= tx_period_us:
+        out_msg = ""
         meas_lin_vel, meas_ang_vel = mobile_base.get_vels()
-        meas_dist = dist_sensor.distance
-        out_msg = f"{meas_lin_vel},{meas_ang_vel},{meas_dist}\n"
-        sys.stdout.write(out_msg)  # main.py will send this to computer
+        out_msg += f"{meas_lin_vel:.4f},{meas_ang_vel:.4f}"
+        meas_mot = motion_sensor.read_data()
+        out_msg += f",{meas_mot['xdd']:.4f},{meas_mot['ydd']:.4f},{meas_mot['zdd']:.4f},{meas_mot['omg_x']:.4f},{meas_mot['omg_y']:.4f},{meas_mot['omg_z']:.4f}"
+        meas_dist = distance_sensor.distance
+        out_msg += f",{meas_dist:.4f}"
+        # out_msg = f"{meas_lin_vel},{meas_ang_vel},{meas_mot['xdd']},{meas_mot['ydd']},{meas_mot['zdd']},{meas_mot['omg_x']},{meas_mot['omg_y']},{meas_mot['omg_z']},{meas_dist}\n"
+        print(out_msg)  # send out_msg to computer
         last_us = now_us  # update last time stamp
     # Receive data (RX)
     is_waiting = pico_messenger.poll(0)  # check data in USB
