5ef249e4a9d07f12733b5eb473406bfe841805e2
linzhanguca <lzhang12@uca.edu>
2026-02-17 21:15:35 -0600

    adopt diff drive controller to uart msngr

--- upython_scripts/uart_messenger.py
+++ upython_scripts/uart_messenger.py
@@ -1,39 +1,38 @@
 from machine import UART, Pin
-from time import time, ticks_ms, ticks_diff
+from time import time, ticks_us, ticks_diff
+from diff_drive_controller import DiffDriveController
 
-uart0 = UART(0, baudrate=115200, tx=Pin(0), rx=Pin(1))
 
-# txData = b"hello world\n\r"
-# uart0.write(txData)
-# time.sleep(0.1)
-# rxData = bytes()
-# while uart0.any() > 0:
-#     rxData += uart0.read(1)
-#
-# print(rxData.decode("utf-8"))
+# SETUP
+uart_msngr = UART(0, baudrate=115200, tx=Pin(0), rx=Pin(1))
+mobile_base = DiffDriveController(
+    left_wheel_ids=((16, 17, 18), (19, 20)),
+    right_wheel_ids=((15, 14, 13), (12, 11)),
+)
+mobile_base.awaken()
 
 # Variables
-tx_interval_ms = 100  # 10Hz
-last_ms = ticks_ms()
-msg_id = 0
-print("UART0 is ready...")
+tx_period_us = 16_667  # 60Hz
+targ_lin_vel, targ_ang_vel = 0.0, 0.0
+last_us = ticks_us()
+# print("UART0 is ready...")  # debug
 
 while True:
     # Transmit data (TX)
-    now_ms = ticks_ms()
-    if ticks_diff(now_ms, last_ms) >= tx_interval_ms:
-        out_msg = f"[Pico, {now_ms}]: Hello, {msg_id}\n\r".encode("utf-8")
-        uart0.write(out_msg)  # main.py will send this to computer
-        msg_id += 1
-        last_ms = now_ms  # update last time stamp
+    now_us = ticks_us()
+    if ticks_diff(now_us, last_us) >= tx_period_us:
+        meas_lin_vel, meas_ang_vel = mobile_base.get_vels()
+        out_msg = f"{meas_lin_vel:.4f},{meas_ang_vel:.4f}\n".encode("utf-8")
+        uart_msngr.write(out_msg)  # main.py will send this to computer
+        last_us = now_us  # update last time stamp
     # Receive data (RX)
-    if uart0.any():
+    if uart_msngr.any():
         # Read all available bytes
-        in_data = uart0.readline()
-        try:
-            # Decode bytes to string and strip whitespace/newlines
-            in_msg = in_data.decode("utf-8").strip()
-            if in_msg:
-                print(f"Received: {in_msg}")
-        except UnicodeError:
-            print("Received non-UTF-8 data")
+        in_data = uart_msngr.readline()
+        in_msg = in_data.decode("utf-8").strip()  # strip whitespace
+        targ_vels = in_msg.split(",")  # get a list
+        print(targ_vels)
+        if len(targ_vels) == 2:
+            targ_lin_vel = float(targ_vels[0])
+            targ_ang_vel = float(targ_vels[1])
+            mobile_base.set_vels(targ_lin_vel, targ_ang_vel)
